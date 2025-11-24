1f814b9d1b4f9659757c436b8e55ee5e794ebcff
Hasanul Haque Shiblu <123184569+hhshiblu@users.noreply.github.com>
2025-11-24 23:11:32 +0600

    disable middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,52 +1,56 @@
-import {
-  apiAuthPrefix,
-  authRoutes,
-  DEFAULT_LOGIN_REDIRECT_URL,
-  publicRoutes,
-} from "./route";
-import NextAuth from "next-auth";
-import authConfig from "./auth.config";
+// import {
+//   apiAuthPrefix,
+//   authRoutes,
+//   DEFAULT_LOGIN_REDIRECT_URL,
+//   publicRoutes,
+// } from "./route";
+// import { NextResponse } from "next/server";
+// import { auth } from "./auth";
+
 import { NextResponse } from "next/server";
-const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
-  const { nextUrl } = req;
-  const isLoggedIn = !!req.auth;
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+// export default auth((req) => {
+//   const { nextUrl } = req;
+//   const isLoggedIn = !!req.auth;
+//   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+//   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+//   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  if (isApiAuthRoute) {
-    return null;
-  }
+//   if (isApiAuthRoute) {
+//     return null;
+//   }
 
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return NextResponse.redirect(
-        new URL(DEFAULT_LOGIN_REDIRECT_URL, nextUrl)
-      );
-    }
-    return null;
-  }
+//   if (isAuthRoute) {
+//     if (isLoggedIn) {
+//       return NextResponse.redirect(
+//         new URL(DEFAULT_LOGIN_REDIRECT_URL, nextUrl)
+//       );
+//     }
+//     return null;
+//   }
 
-  if (!isLoggedIn && !isPublicRoute) {
-    let callbackUrl = nextUrl.pathname;
-    if (nextUrl.search) {
-      callbackUrl += nextUrl.search;
-    }
-    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-    return NextResponse.redirect(
-      new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
-    );
-  }
+//   if (!isLoggedIn && !isPublicRoute) {
+//     let callbackUrl = nextUrl.pathname;
+//     if (nextUrl.search) {
+//       callbackUrl += nextUrl.search;
+//     }
+//     const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+//     return NextResponse.redirect(
+//       new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+//     );
+//   }
 
-  return null;
-});
+//   return null;
+// });
 
-export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
-};
-// export default auth((req) => {});
+// export const config = {
+//   matcher: [
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     "/(api|trpc)(.*)",
+//   ],
+// };
+// // export default auth((req) => {});
+
+export default function middleware(req) {
+  return NextResponse.next();
+}
